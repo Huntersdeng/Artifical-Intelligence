954399fdfdbcd3c1d9cd5dc055f13a5b7a82d4f5
Huntersdeng <1050130028@qq.com>
2018-11-16 20:08:57 +0800

    homework4

--- homework4/BayesNetworkInference.py
+++ homework4/BayesNetworkInference.py
@@ -1,47 +1,92 @@
-from BayesianNetworks import *
+from BayesianNetworks import inference, readFactorTablefromData
 import numpy as np
 import pandas as pd
+from copy import deepcopy
 
 class riskFactorNetwork:
-    def __init__(self, data):
-        riskFactorNet = pd.read_csv(data)
-        ## create factors
-        self.income      = readFactorTablefromData(riskFactorNet, ['income'])
-        self.smoke       = readFactorTablefromData(riskFactorNet, ['smoke', 'income'])
-        self.exercise    = readFactorTablefromData(riskFactorNet, ['exercise', 'income'])
-        self.bmi         = readFactorTablefromData(riskFactorNet, ['bmi', 'income'])
-        self.bp          = readFactorTablefromData(riskFactorNet, ['bp', 'exercise', 'income', 'smoke'])
-        self.cholesterol = readFactorTablefromData(riskFactorNet, ['cholesterol', 'exercise', 'income', 'smoke'])
-        self.diabetes    = readFactorTablefromData(riskFactorNet, ['diabetes', 'bmi'])
-        self.stroke      = readFactorTablefromData(riskFactorNet, ['stroke', 'bmi', 'bp', 'cholesterol'])
-        self.attack      = readFactorTablefromData(riskFactorNet, ['attack', 'bmi', 'bp', 'cholesterol'])
-        self.angina      = readFactorTablefromData(riskFactorNet, ['angina', 'bmi', 'bp', 'cholesterol'])
-        self.vars = ['income', 'smoke', 'exercise', 'bmi', 'bp', 'stroke', 'attack', 'angina', 'cholesterol', 'diabetes']
-        self.risk_net = [self.income, self.smoke, self.exercise, self.bmi, 
-                         self.bp, self.cholesterol, self.diabetes, self.stroke,
-                         self.attack, self.angina]
+    def __init__(self):
+        self._riskFactorNet = pd.read_csv('RiskFactorsData.csv')
+        self._connection = {'income':['income'], 'smoke':['smoke', 'income'], 'exercise':['exercise', 'income'], 
+                           'bmi':['bmi', 'income'], 'bp':['bp', 'exercise', 'income', 'smoke'],
+                           'cholesterol':['cholesterol', 'exercise', 'income', 'smoke'],'diabetes':['diabetes', 'bmi'], 
+                           'stroke':['stroke', 'bmi', 'bp', 'cholesterol'], 'attack':['attack', 'bmi', 'bp', 'cholesterol'], 
+                           'angina':['angina', 'bmi', 'bp', 'cholesterol']}
+        self._factor_dict = {}
+        for node,parent_nodes in self._connection.items():
+            self._factor_dict[node] = readFactorTablefromData(self._riskFactorNet, parent_nodes)
 
+    def get_network(self):
+        return [factor for factor in self._factor_dict.values()]
+        
     def size(self):
         size = 0
-        for factor in self.risk_net:
+        for factor in self._factor_dict.values():
             size += factor.iloc[:,0].size
         return size
 
+    def change_edge(self, parent_nodes, nodes):
+        for parent_node in parent_nodes:
+            for node in nodes:
+                self._connection[node].append(parent_node)
+                self._factor_dict[node] = readFactorTablefromData(self._riskFactorNet, self._connection[node])
+
     def infer(self, outcome, evidenceVars, evidenceVals):
-        hiddenVar = self.vars.copy()
+        risk_net = self.get_network()
+        hiddenVar = [var for var in self._factor_dict]
+        
         hiddenVar.remove(outcome)
         for var in evidenceVars:
             hiddenVar.remove(var)
-        return inference(self.risk_net, hiddenVar, evidenceVars, evidenceVals)
+        return inference(risk_net, hiddenVar, evidenceVars, evidenceVals)
 
-if __name__=='__main__':
-    Network = riskFactorNetwork('RiskFactorsData.csv')
+def question1(Network):
     print('the size of this network is ', Network.size())
-    outcomes = ['diabetes', 'stroke', 'attack', 'angina']
+
+def question2(Network, outcomes):
     print('\nIf I have bad habits:')
     for outcome in outcomes:
         print('The probabilities of the ', outcome, ' is\n' ,Network.infer(outcome, ['smoke', 'exercise'], [1,2]))
     print('\nIf I have good habits:')
     for outcome in outcomes:
         print('The probabilities of the ', outcome, ' is\n' ,Network.infer(outcome, ['smoke', 'exercise'], [2,1]))
+    print('\nIf I have poor health:')
+    for outcome in outcomes:
+        print('The probabilities of the ', outcome, ' is\n' ,Network.infer(outcome, ['bp', 'cholesterol', 'bmi'], [1,1,3]))
+    print('\nIf I have good health:')
+    for outcome in outcomes:
+        print('The probabilities of the ', outcome, ' is\n' ,Network.infer(outcome, ['bp', 'cholesterol', 'bmi'], [3,2,2]))
+
+def question3(Network, outcomes):
+    probs = {}
+    index={0:'diabetes', 1:'stroke', 2:'attack', 3:'angina'}
+    for i in range(1,9):
+        probs[str(i)]=[]
+        for outcome in outcomes:
+            factor = Network.infer(outcome, ['income'], [i])
+            probs[str(i)].append(float(factor[factor[outcome]==1]['probs']))
+    df = pd.DataFrame(probs)
+    df = df.rename(index, axis='index')
+    print(df)
+
+def question4(Network, outcomes):
+    Network2 = deepcopy(Network)
+    Network2.change_edge(['smoke', 'exercise'], outcomes)
+    question2(Network2, outcomes)
+    return Network2
+
+def question5(Network2, outcomes):
+    Network3 = deepcopy(Network2)
+    Network3.change_edge(['diabetes'], ['stroke'])
+    print(Network3.infer('stroke', ['diabetes'], [1]))
+    print(Network3.infer('stroke', ['diabetes'], [3]))
+
+if __name__=='__main__':
+    Network = riskFactorNetwork()
+    outcomes = ['diabetes', 'stroke', 'attack', 'angina']
+    question1(Network)
+    question2(Network, outcomes)
+    question3(Network, outcomes)
+    Network2 = question4(Network, outcomes)
+    question5(Network2, outcomes)
+    
     
--- homework4/BayesianNetworks.py
+++ homework4/BayesianNetworks.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from functools import reduce
+from copy import deepcopy
 ## Function to create a conditional probability table
 ## Conditional probability is of the form p(x1 | x2, ..., xk)
 ## varnames: vector of variable names (strings) first variable listed 
@@ -211,3 +212,89 @@ def normalize(Factor):
     s = reduce(lambda x,y:x+y,list(Factor['probs']))
     Factor['probs']/=s
     return Factor
+
+class riskFactorNetwork:
+    def __init__(self):
+        self._riskFactorNet = pd.read_csv('RiskFactorsData.csv')
+        self._connection = {'income':['income'], 'smoke':['smoke', 'income'], 'exercise':['exercise', 'income'], 
+                           'bmi':['bmi', 'income'], 'bp':['bp', 'exercise', 'income', 'smoke'],
+                           'cholesterol':['cholesterol', 'exercise', 'income', 'smoke'],'diabetes':['diabetes', 'bmi'], 
+                           'stroke':['stroke', 'bmi', 'bp', 'cholesterol'], 'attack':['attack', 'bmi', 'bp', 'cholesterol'], 
+                           'angina':['angina', 'bmi', 'bp', 'cholesterol']}
+        self._factor_dict = {}
+        for node,parent_nodes in self._connection.items():
+            self._factor_dict[node] = readFactorTablefromData(self._riskFactorNet, parent_nodes)
+
+    def get_network(self):
+        return [factor for factor in self._factor_dict.values()]
+        
+    def size(self):
+        size = 0
+        for factor in self._factor_dict.values():
+            size += factor.iloc[:,0].size
+        return size
+
+    def change_edge(self, parent_nodes, nodes):
+        for parent_node in parent_nodes:
+            for node in nodes:
+                self._connection[node].append(parent_node)
+                self._factor_dict[node] = readFactorTablefromData(self._riskFactorNet, self._connection[node])
+
+    def infer(self, outcome, evidenceVars, evidenceVals):
+        risk_net = self.get_network()
+        hiddenVar = [var for var in self._factor_dict]
+        
+        hiddenVar.remove(outcome)
+        for var in evidenceVars:
+            hiddenVar.remove(var)
+        return inference(risk_net, hiddenVar, evidenceVars, evidenceVals)
+
+def question1(Network):
+    print('the size of this network is ', Network.size())
+
+def question2(Network, outcomes):
+    print('\nIf I have bad habits:')
+    for outcome in outcomes:
+        print('The probabilities of the ', outcome, ' is\n' ,Network.infer(outcome, ['smoke', 'exercise'], [1,2]))
+    print('\nIf I have good habits:')
+    for outcome in outcomes:
+        print('The probabilities of the ', outcome, ' is\n' ,Network.infer(outcome, ['smoke', 'exercise'], [2,1]))
+    print('\nIf I have poor health:')
+    for outcome in outcomes:
+        print('The probabilities of the ', outcome, ' is\n' ,Network.infer(outcome, ['bp', 'cholesterol', 'bmi'], [1,1,3]))
+    print('\nIf I have good health:')
+    for outcome in outcomes:
+        print('The probabilities of the ', outcome, ' is\n' ,Network.infer(outcome, ['bp', 'cholesterol', 'bmi'], [3,2,2]))
+
+def question3(Network, outcomes):
+    probs = {}
+    index={0:'diabetes', 1:'stroke', 2:'attack', 3:'angina'}
+    for i in range(1,9):
+        probs[str(i)]=[]
+        for outcome in outcomes:
+            factor = Network.infer(outcome, ['income'], [i])
+            probs[str(i)].append(float(factor[factor[outcome]==1]['probs']))
+    df = pd.DataFrame(probs)
+    df = df.rename(index, axis='index')
+    print(df)
+
+def question4(Network, outcomes):
+    Network2 = deepcopy(Network)
+    Network2.change_edge(['smoke', 'exercise'], outcomes)
+    question2(Network2, outcomes)
+    return Network2
+
+def question5(Network2, outcomes):
+    Network3 = deepcopy(Network2)
+    Network3.change_edge(['diabetes'], ['stroke'])
+    print(Network3.infer('stroke', ['diabetes'], [1]))
+    print(Network3.infer('stroke', ['diabetes'], [3]))
+
+if __name__=='__main__':
+    Network = riskFactorNetwork()
+    outcomes = ['diabetes', 'stroke', 'attack', 'angina']
+    question1(Network)
+    question2(Network, outcomes)
+    question3(Network, outcomes)
+    Network2 = question4(Network, outcomes)
+    question5(Network2, outcomes)
